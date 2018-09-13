8977a91dfce96d36e78c49d2e34a7780b27e577c
Annah Isenberg <annahisenberg@gmail.com>
2018-09-13 16:39:38 -0400

    add all blog router tests

--- config.js
+++ config.js
@@ -3,5 +3,6 @@
 module.exports = {
     CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
     DATABASE_URL: process.env.DATABASE_URL || 'mongodb://localhost/blog_capstone_db',
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'mongodb://localhost/test_blog_capstone_db',
     PORT: process.env.PORT || 4000
 };
