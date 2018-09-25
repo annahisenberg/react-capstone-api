34bb78f377d1f7f07801a0d24f2d0a33f79e44a2
Annah Isenberg <annahisenberg@gmail.com>
2018-09-25 11:33:25 -0500

    Add user model and user-router files

---
+++ models/user-model.js
@@ -0,0 +1,19 @@
+'use strict';
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+const UserSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
